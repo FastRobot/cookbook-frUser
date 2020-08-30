6b4b917799522ca55b6a86142a85b3980580246c
lamont <lamont@fastrobot.com>
2020-08-29 20:08:07 -0700

    adding a remote role for eric

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer       'Fast Robot, LLC'
 maintainer_email 'cookbooks@fastrobot.com'
 license          'Apache-2.0'
 description      'Installs/Configures FastRobot designated users'
-version          '0.2.1'
+version          '0.2.2'
 
 %w( ubuntu debian redhat centos fedora freebsd).each do |os|
   supports os
